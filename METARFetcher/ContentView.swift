bfc4be6e3ad115b811e946627388e7f882738e63
Benjamin Montgomery <bmontgom@montynet.org>
2020-01-21 13:13:56 -0500

    Initial Commit

---
+++ METARFetcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  METARFetcher
+//
+//  Created by Benjamin Montgomery on 1/21/20.
+//  Copyright © 2020 Benjamin Montgomery. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
